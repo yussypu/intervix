11914a52b18a1fb3e53b601103e71fecee8b309e
Yahya Ehsan <124023463+yussypu@users.noreply.github.com>
2024-08-19 05:11:08 +0500

    Create GraphsCourse.tsx

---
+++ src/components/courses/GraphsCourse.tsx
@@ -0,0 +1,18 @@
+const GraphsCourse = () => {
+	return (
+		<div className='p-6'>
+			<h2 className='text-2xl font-bold text-white mb-4'>Graphs</h2>
+			<p className='text-lg text-white'>
+				Graphs are abstract data structures that consist of nodes (vertices) and edges. They are used to model relationships and networks.
+			</p>
+			<h3 className='text-xl font-bold text-white mt-6'>Topics Covered</h3>
+			<ul className='text-lg text-white'>
+				<li>Graph Representations</li>
+				<li>Graph Traversal Algorithms</li>
+				<li>Applications and Problems</li>
+			</ul>
+		</div>
+	);
+};
+
+export default GraphsCourse;
