a508eaf4792ad7aa62a41513ec7533a589f3fe0b
Yahya Ehsan <124023463+yussypu@users.noreply.github.com>
2024-08-20 10:51:02 +0500

    Update problems.ts

--- src/mockProblems/problems.ts
+++ src/mockProblems/problems.ts
@@ -16,22 +16,7 @@ export const problems: Problem[] = [
 		order: 1,
 		videoId: "8-k1C6ehKuw",
 	},
-	{
-		id: "reverse-linked-list",
-		title: "Reverse Linked List",
-		difficulty: "Hard",
-		category: "Linked List",
-		order: 2,
-		videoId: "",
-	},
-	{
-		id: "jump-game",
-		title: "Jump Game",
-		difficulty: "Medium",
-		category: "Dynamic Programming",
-		order: 3,
-		videoId: "",
-	},
+
 	{
 		id: "valid-parentheses",
 		title: "Valid Parentheses",
@@ -40,38 +25,7 @@ export const problems: Problem[] = [
 		order: 4,
 		videoId: "xty7fr-k0TU",
 	},
-	{
-		id: "search-a-2d-matrix",
-		title: "Search a 2D Matrix",
-		difficulty: "Medium",
-		category: "Binary Search",
-		order: 5,
-		videoId: "ZfFl4torNg4",
-	},
-	{
-		id: "container-with-most-water",
-		title: "Container With Most Water",
-		difficulty: "Medium",
-		category: "Two Pointers",
-		order: 6,
-		videoId: "",
-	},
-	{
-		id: "merge-intervals",
-		title: "Merge Intervals",
-		difficulty: "Medium",
-		category: "intervals",
-		order: 7,
-		videoId: "",
-	},
-	{
-		id: "maximum-depth-of-binary-tree",
-		title: "Maximum Depth of Binary Tree",
-		difficulty: "Easy",
-		category: "Tree",
-		order: 8,
-		videoId: "4qYTqOiRMoM",
-	},
+
 	{
 		id: "best-time-to-buy-and-sell-stock",
 		title: "Best Time to Buy and Sell Stock",
@@ -80,12 +34,5 @@ export const problems: Problem[] = [
 		order: 9,
 		videoId: "",
 	},
-	{
-		id: "subsets",
-		title: "Subsets",
-		difficulty: "Medium",
-		category: "Backtracking",
-		order: 10,
-		videoId: "",
-	},
+
 ];
