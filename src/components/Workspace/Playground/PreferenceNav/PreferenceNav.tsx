41b9a58a75b543f5cd2ea8bea74c107af653f17b
Yahya Ehsan <124023463+yussypu@users.noreply.github.com>
2024-10-05 16:04:34 +0200

    Update PreferenceNav.tsx

--- src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -42,7 +42,7 @@ const PreferenceNav: React.FC<PreferenceNavProps> = ({ setSettings, settings }) 
 			<div className='flex items-center text-white'>
 				<button className='flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2  px-2 py-1.5 font-medium'>
 					<div className='flex items-center px-1'>
-						<div className='text-xs text-label-2 dark:text-dark-label-2'>JavaScript</div>
+						<div className='text-xs text-label-2 dark:text-dark-label-2'>CSS</div>
 					</div>
 				</button>
 			</div>
